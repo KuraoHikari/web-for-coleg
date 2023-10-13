443894f4dfdf4cba36ec0cd82ca69181847cbf55
KuraoHikari <dewaindra705@gmail.com>
2023-10-13 13:46:59 +0800

    first commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:123@localhost:5932/nestjs?schema=public',
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'production') return;
+
+    // teardown logic
+    return Promise.all([this.user.deleteMany()]);
+  }
+}
